7b3343882da0b38dcb084772246cd0feea82cc08
AntoineCARON <antoinecaron.simplon@gmail.com>
2017-12-20 09:47:48 +0100

    progress

---
+++ js/app.js
@@ -0,0 +1,25 @@
+(function main(){
+	$('#idbtn1').click(function boucle(){
+		// console.log('#idbtn1');
+		var array = [2,4,6];
+		// console.log(array);
+		for (i=0 ; i<array.length ; i++){
+			array[i]=array[i]*array[i];
+		}
+		alert(array);
+	});
+	$('#idbtn2').click(function boucle2(){
+		var array = [''];
+		var longueur = prompt('');
+		if {
+			for (i=0 ; i<51 ; i++){
+				// if (longueur.toLowerCase() == '');
+				prompt[i] = longueur[i]*longueur[i];
+			}
+		}
+		alert(prompt[i]);
+	});
+
+
+
+})();
